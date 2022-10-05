3585320bd552743b6d34dc8deb13efd868eb48c9
JustWaveThings <justwavethings@proton.me>
2022-10-05 14:48:51 -0400

    Add eventListeners to squares with once attribute

--- ttt.js
+++ ttt.js
@@ -3,7 +3,20 @@
 /* Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker. Don’t forget the logic that keeps players from playing in spots that are already taken!
 Think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects.. but take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later! */
 
-const GameBoard = (function () {})();
+const GameBoard = (function () {
+	const squares = document.querySelectorAll('.space');
+	squares.forEach((squares) => {
+		squares.addEventListener(
+			'click',
+			(e) => {
+				console.log(squares.id);
+			},
+			{
+				once: true,
+			}
+		);
+	});
+})();
 
 const Player = (name, marker) => {
 	return { name, marker };
